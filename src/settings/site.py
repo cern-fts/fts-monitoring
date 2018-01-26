7e4298918495db5dd7c24bb0fc236cc09ebae19e
Maria Arsuaga Rios <maria.arsuaga.rios@cern.ch>
2018-01-26 16:41:06 +0100

    Adding monit variable in detail optimizer

--- src/settings/site.py
+++ src/settings/site.py
@@ -37,6 +37,12 @@ ADMINS = (
     (FTS3WEB_CONFIG.get('site', 'admin_name'), FTS3WEB_CONFIG.get('site', 'admin_mail'))
 )
 
+if FTS3WEB_CONFIG.get('site', 'monit'):
+    
+    SITE_MONIT = FTS3WEB_CONFIG.get('site', 'monit')
+else:
+    SITE_MONIT = none
+
 MANAGERS = ADMINS
 
 if FTS3WEB_CONFIG.get('logs', 'port'):
