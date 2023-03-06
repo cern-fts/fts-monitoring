bba9f1aa2d95975842d322027ac02fe72194c0bf
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2023-03-06 17:21:11 +0100

    FTS-1886: Display FTS Web Monitoring version in the header bar

--- src/settings/version.py
+++ src/settings/version.py
@@ -1,9 +1,6 @@
 # Copyright notice:
 #
-# Copyright (C) CERN 2013-2015
-#
-# Copyright (C) Members of the EMI Collaboration, 2010-2013.
-#   See www.eu-emi.eu for details on the copyright holders
+# Copyright (C) CERN 2023
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,9 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .cache import *
-from .common import *
-from .database import *
-from .format import *
-from .site import *
-from .version import *
+# The FTS Web Monitoring version:
+# - Displayed in the header
+# - Returned in the "/version" endpoint
+VERSION = "3.12.1"
