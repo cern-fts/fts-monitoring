5b592001fa86a99e48282cbbbecdc0a778792986
Joao Lopes <joao.pedro.batista.lopes@cern.ch>
2023-09-18 16:50:09 +0200

    CI: Do not install python2 with the fedora-packages.sh helper script

--- ci/fedora-packages.sh
+++ ci/fedora-packages.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [[ -f /usr/bin/dnf ]]; then
-  dnf install -y dnf-plugins-core git rpm-build make tree which python2
+  dnf install -y dnf-plugins-core git rpm-build make tree which
 else
-  yum install -y yum-utils git rpm-build make tree which python2
+  yum install -y yum-utils git rpm-build make tree which
 fi
