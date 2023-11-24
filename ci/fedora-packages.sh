d6df33ff80f03c90c07c3b06d63299d92e1075fd
Joao Lopes <joao.pedro.batista.lopes@cern.ch>
2023-11-24 16:42:58 +0100

    CI: Install epel-rpm-macros package before building rpms

--- ci/fedora-packages.sh
+++ ci/fedora-packages.sh
@@ -4,5 +4,5 @@ set -e
 if [[ -f /usr/bin/dnf ]]; then
   dnf install -y dnf-plugins-core git rpm-build make tree which
 else
-  yum install -y yum-utils git rpm-build make tree which
+  yum install -y yum-utils git rpm-build make tree which epel-rpm-macros
 fi
