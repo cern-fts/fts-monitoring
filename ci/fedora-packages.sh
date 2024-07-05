08da80fa58db2a6f39fd12675ddf9b58f9eeaa51
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2024-07-05 10:16:16 +0200

    FTS-2029: Remove references to "yum" from the build infrastructure

--- ci/fedora-packages.sh
+++ ci/fedora-packages.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-if [[ -f /usr/bin/dnf ]]; then
-  dnf install -y dnf-plugins-core git rpm-build make tree which
-else
-  yum install -y yum-utils git rpm-build make tree which epel-rpm-macros
+# Ensure "epel-release" package is installed
+if ! rpm -q --quiet epel-release ; then
+  dnf install -y epel-release
 fi
+
+dnf install -y dnf-plugins-core git rpm-build make tree which
